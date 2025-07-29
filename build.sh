fc1add5cc127503c9f1611bf5888fd34d80d0468
JoseNoeT <jose.miguel.n.t.2022@gmail.com>
2025-07-28 23:35:55 -0400

    Fix: Ajuste rutas build.sh para rootDir backend

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Entrar en la carpeta donde está manage.py
-cd backend
-
-# Instalar dependencias
+# Ya estamos dentro de /backend gracias al rootDir
 pip install -r ../requirements.txt
-
-# Migrar base de datos
 python manage.py migrate
-
-# Recolectar estáticos
 python manage.py collectstatic --noinput
