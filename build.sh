3755ed441559077b862e6cc350ffc1abf5c28805
JoseNoeT <jose.miguel.n.t.2022@gmail.com>
2025-07-28 23:49:14 -0400

    Fix: ruta correcta para requirements.txt

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Instalar dependencias desde la raíz del repo
-pip install -r $RENDER_PROJECT_ROOT/requirements.txt
+# Instalar dependencias (subimos un nivel porque requirements.txt está en la raíz del repo)
+pip install -r ../requirements.txt
 
-# Migrar y recolectar estáticos (ya estamos en backend)
+# Migrar la base de datos
 python manage.py migrate
+
+# Recolectar archivos estáticos
 python manage.py collectstatic --noinput
